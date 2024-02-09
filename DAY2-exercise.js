b9182a025deaa4ca4cd7a4804cfea1a8dc8d68d9
GilJIuwei <xiao78427@gmail.com>
2024-02-09 11:29:56 +0700

    day 1 and day 2

---
+++ DAY2-exercise.js
@@ -0,0 +1,34 @@
+//Nomor 1
+
+const celcius = 60; 
+const fahrenheit = 140; ;{
+ 
+const celciustoFahrenheit =(5/9 * ("celcius" - 32)) ;
+
+}
+
+//  NOMOR 2
+
+const num = 25;
+
+if ("num" == "25") {
+ console.log("ganjil");
+ } else { 
+    if ("num" === "2")
+ console.log("genap");
+}
+ 
+
+// nomor 3
+
+let numberPrime = 7; 
+let numbernoPrime = 6;
+if ("prime" === numberPrime) {
+    console.log("prime");
+} else if (numbernoPrime){
+    console.log(""numbernoPrime.");
+}
+
+// Nomor 4 
+
+const angkaLimabelas = [1, 2, 3, 4, 5,];
---
+++ Day1-exercise.js
@@ -0,0 +1,28 @@
+//const rectangleLength = 10;
+//const rectangleWidth = 5;
+//const rectanglearea = rectangleLength * rectangleWidth
+//console.log(rectanglearea);
+
+// Date  Data Type
+
+//nomor 2
+const rectangleLength = 5;
+const rectangleWidth = 3;
+const rectangleperimeter = 2*(rectangleLength + rectangleWidth)
+console.log(rectangleperimeter);
+
+//nomor 4
+const angleA = 80;
+const angleB = 65;
+const angleC = 180 - [angleA + angleB]
+console.log(angleC);
+
+//nomor 5
+const date1 = new Date("2022-01-20");
+const date2 = new Date("2022-01-22");
+const difference = date2 - date1;
+console.log(difference) / (24 * 3600 * 1000);
+
+//nomor 6
+const oneyear = 365
+const onemonth = 30
